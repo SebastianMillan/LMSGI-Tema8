2f44093acd7786b4ae5a7523cb095bfc70bfc862
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-04-13 10:02:20 +0200

    INSTITUTO - Seguir ejemplo

--- Ejemplos/ejemplo2.js
+++ Ejemplos/ejemplo2.js
@@ -1,3 +1,9 @@
+let boton2 = document.getElementById("boton2");
+boton2.addEventListener("click",crearParrafo);
+
+let boton1 = document.getElementById("boton1");
+boton1.addEventListener("click",crearAlumno);
+
 function buscarEnlaces(){
     let enlace= document.getElementById("enlace");
     alert(enlace.innerHTML);
@@ -16,13 +22,24 @@ function buscarEnlacesPorSelector(){
 buscarEnlaces();
 buscarEnlacesPorSelector();
 
-function crearParrafo(contenido){
+function crearParrafo(){
     let parrafoNuevo = document.createElement("p");
+    let contenido = prompt("Indica el texto del párrafo nuevo");
     parrafoNuevo.innerHTML = contenido;
 
     let elBody = document.querySelector("body");
     elBody.append(parrafoNuevo);
 }
 
-crearParrafo(prompt("Indica el texto del párrafo nuevo"));
+let indice = 1;
+
+function crearAlumno(){
+    let ultimaFila = document.querySelector("tr:last-child");
+    let nuevaFila = ultimaFila.cloneNode(true);
+    nuevaFila.firstElementChild.innerHTML = prompt("Indique el nombre del Alumno");
+    nuevaFila.lastElementChild.innerHTML = ++indice;
+    document.querySelector("table").append(nuevaFila);
+}
+
+
 
